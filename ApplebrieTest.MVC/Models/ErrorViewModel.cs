769474b2b5dbb2fc5a3ddef10cd13f8025ff84ca
Andranik Khachatryan <53318183+antosh0077@users.noreply.github.com>
2022-05-23 15:06:30 +0400

    Delete ApplebrieTest.MVC directory
    Delete MVC proj

--- ApplebrieTest.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ApplebrieTest.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
